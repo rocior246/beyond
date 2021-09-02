9d39416a75df1e6aed705700001d12c8963e3335
Rocior246 <rocior246@yahoo.com>
2021-09-02 14:34:00 -0500

    Ejercicios Capitulo1

---
+++ Capitulo1/abcTraceback.py
@@ -0,0 +1,13 @@
+def a():
+    print('Start of a()')
+    b()  # Call b().
+
+def b():
+    print('Start of b()')
+    c()  # Call c().
+
+def c():
+    print('Start of c()')
+    42 / 0  # This will cause a zero divide error.
+
+a()  # Call a().
---
+++ Capitulo1/zeroDivideTraceback.py
@@ -0,0 +1,8 @@
+#ENFRENTAR ERRORES Y PEDIR AYUDA
+def spam(number1, number2):
+    return number1 / (number2 - 42)
+
+spam(101, 42)
+
+#Lint software, or linters, are applications that analyze your source code to warn you of any potential errors.
+#static analysis (examining source code without running it) can identify common errors caused by typos, so you can see the line on which they occur.
